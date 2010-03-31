f55fbe232d748fd5cf7205c4fe186bd047438d6e
Igor Kupczyński <puszczyk@gmail.com>
2010-03-31 15:52:26 +0200

    Started working on new parser. First new expression parser

---
+++ Darwin/src/pl/poznan/put/darwin/model/problem/ProblemADT.scala
@@ -0,0 +1,25 @@
+package pl.poznan.put.darwin.model.problem
+
+/**
+ * Module containing abstract data types for problem.
+ *
+ * @author: Igor Kupczynski
+ */
+
+
+abstract class Expr
+
+/* Leafs */
+case class Constant(value: Double) extends Expr
+case class Variable(name: String, min: Double, max: Double) extends Expr
+case class Interval(name: String, lower: Double, upper: Double) extends Expr {
+  def getMiddleValue(): Double = {
+    lower + ((upper - lower) / 2)
+  }
+}
+
+/* Operators */
+case class UnaryOp(operator: String, arg: Expr) extends Expr
+case class BinaryOp(operator: String, lhs: Expr, rhs: Expr) extends Expr
+  
+
--- Darwin/test/pl/poznan/put/darwin/model/problem/ProblemADTTest.scala
+++ Darwin/test/pl/poznan/put/darwin/model/problem/ProblemADTTest.scala
@@ -1,11 +1,39 @@
 package pl.poznan.put.darwin.model.problem
 
-/**
- * Created by IntelliJ IDEA.
- * User: puszczyk
- * Date: Mar 31, 2010
- * Time: 3:27:15 PM
- * To change this template use File | Settings | File Templates.
- */
+class ProblemADTTest {
+  import org.junit._, Assert._
 
-class ProblemADTTest
+  @Test def LeafTest = {
+    val c1 = Constant(-15)
+    assertEquals(-15.0, c1.value, 0.0)
+
+    val v1 = Variable("x1", 0, 1)
+    assertEquals("x1", v1.name)
+    assertEquals(0.0, v1.min, 0.0)
+    assertEquals(1.0, v1.max, 0.0)
+
+    val i1 = Interval("limit1", -1, 10)
+    assertEquals("limit1", i1.name)
+    assertEquals(-1.0, i1.lower, 0.0)
+    assertEquals(10.0, i1.upper, 0.0)
+    assertEquals(4.5, i1.getMiddleValue(), 0.0)
+
+    val i2 = Interval("limit2", 3.14, 3.14)
+    assertEquals("limit2", i2.name)
+    assertEquals(3.14, i2.lower, 0.0)
+    assertEquals(3.14, i2.upper, 0.0)
+    assertEquals(3.14, i2.getMiddleValue(), 0.0)
+  }
+
+  @Test def OperatorTest = {
+    val u1 = UnaryOp("-", Constant(10.0))
+    assertEquals("-", u1.operator)
+    assertEquals(Constant(10.0), u1.arg)
+
+    val b1 = BinaryOp("+", Constant(5.0), Constant(15.0))
+    assertEquals("+", b1.operator)
+    assertEquals(Constant(5.0), b1.lhs)
+    assertEquals(Constant(15.0), b1.rhs)
+  }
+
+}
