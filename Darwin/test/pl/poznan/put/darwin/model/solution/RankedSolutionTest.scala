cc5bb3252a372bf522e417b9cc154d726471a6ef
Igor Kupczyński <puszczyk@gmail.com>
2010-04-23 01:12:41 +0200

    Integration with specs is now better

--- Darwin/test/pl/poznan/put/darwin/model/solution/RankedSolutionTest.scala
+++ Darwin/test/pl/poznan/put/darwin/model/solution/RankedSolutionTest.scala
@@ -1,14 +1,16 @@
 package pl.poznan.put.darwin.model.solution
 
 import org.specs._
+import org.specs.runner._
 import mock.Mockito
 import org.mockito.Matchers._
 import pl.poznan.put.darwin.model.problem.{VariableDef, Problem, Variable, Goal}
 import pl.poznan.put.cs.idss.jrs.rules.{Rule, RulesContainer}
-import pl.poznan.put.cs.idss.jrs.types.Example
 import java.util.ArrayList
+import pl.poznan.put.cs.idss.jrs.types.{FloatField, Field, Example}
 
-class RankedSolutionTest extends SpecificationWithJUnit with Mockito {
+
+class RankedSolutionTest extends SpecificationWithJUnit with ScalaTest with Mockito {
 
   val maxX = Goal("x", Variable("x"), true)
   val minY = Goal("y", Variable("y"), false)
@@ -19,26 +21,40 @@ class RankedSolutionTest extends SpecificationWithJUnit with Mockito {
                       null,
                       List())
 
-  val sols = List((5.0, 20.0), (10.0, 15.0), (10.0, 20.0), (5.0, 15.0)).map[EvaluatedSolution]({case (x, y) => 
+  val sols = List((1.0, 4.0), (2.0, 3.0), (2.0, 4.0), (1.0, 3.0)).map[EvaluatedSolution]({case (x, y) =>
     new EvaluatedSolution(p, Map("x" -> x, "y" -> y), Map(maxX -> List(x), minY -> List(y)))
   })
 
-  val ruleX: Rule = mock(Rule)
-  ruleX.covers(any[Example]) answers {e =>
-    println e
-    true
+  val ruleX = mock[Rule]
+  ruleX.covers(any[Example]) answers {e => {
+      val example: Example = e.asInstanceOf[Example]
+      val x: FloatField = example.getField(1).asInstanceOf[FloatField]
+      x.get() >= 2
+    }
   }
-  val ruleY: Rule = mock(Rule)
-  ruleY.covers(any[Example]) answers {e =>
-    println(e)
-    true
+  val ruleY = mock[Rule]
+  ruleY.covers(any[Example]) answers {e => {
+      val example: Example = e.asInstanceOf[Example]
+      val y: FloatField = example.getField(4).asInstanceOf[FloatField]
+      y.get() <= 3
+    }
   }
-  val rules = mock(ArrayList)
-  rules.toArray returns Array[Rule](ruleX, ruleY)
 
-  val rulesContainer = mock(RulesContainer)
-  rulesContainer.getRules(Rule.CERTAIN, Rule.AT_LEAST) returns rules
+  val rulesArray: ArrayList[Rule] = new ArrayList[Rule]()
+  rulesArray.add(ruleX)
+  rulesArray.add(ruleY)
+
+  val rulesContainer = mock[RulesContainer]
+  rulesContainer.getRules(Rule.CERTAIN, Rule.AT_LEAST) returns rulesArray
 
   val rankedSolutions: List[RankedSolution] = RankedSolution(sols, rulesContainer)
 
+  "Pack of Rankend solutions" should {
+    "have a rank value calculated" in {
+      rankedSolutions foreach (s => {
+        println("%s ::: %s, %s, %s" format (s.values, s.rank, s.primaryScore, s.secondaryScore))
+        1 must be_==(1)
+      })
+    }
+  }
 }
