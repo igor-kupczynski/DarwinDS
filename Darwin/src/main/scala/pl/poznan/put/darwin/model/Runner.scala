94e9041872602be2b48e7a0e63eab29819430ed1
Igor Kupczyński <puszczyk@gmail.com>
2010-04-26 16:56:27 +0200

    Changed experiment to simulation

--- Darwin/src/main/scala/pl/poznan/put/darwin/model/Runner.scala
+++ Darwin/src/main/scala/pl/poznan/put/darwin/model/Runner.scala
@@ -2,7 +2,7 @@ package pl.poznan.put.darwin.model
 
 import problem.Parser
 import scala.Iterator.range
-import pl.poznan.put.darwin.experiment.DMMock
+import pl.poznan.put.darwin.simulation.DMMock
 import pl.poznan.put.darwin.evolution.DarwinEvolver
 import solution.{MarkedSolution, EvaluatedSolution, Solution}
 
