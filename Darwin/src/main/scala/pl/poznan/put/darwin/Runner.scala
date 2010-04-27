044f434136a997b0d4c721205c1b28d6adab4e9e
Igor Kupczyński <puszczyk@gmail.com>
2010-04-27 23:34:50 +0200

    Working on reading config from file

--- Darwin/src/main/scala/pl/poznan/put/darwin/Runner.scala
+++ Darwin/src/main/scala/pl/poznan/put/darwin/Runner.scala
@@ -2,12 +2,24 @@ package pl.poznan.put.darwin.model
 
 import pl.poznan.put.darwin.simulation.Simulation
 import pl.poznan.put.darwin.model.problem.{Parser, Problem}
+import org.ini4j.ConfigParser
   
 object Runner {
   def main(args: Array[String]) {
-    val config = new Config()
-    val lines = io.Source.fromPath(config.FILENAME).mkString
+
+    val problemFilename = "etc/problems/simple_1crit.mod"
+    val configFilename = "etc/config.ini"
+
+    val parser = new ConfigParser()
+    parser.read(configFilename)
+
+    println(parser)
+  
+    val config = new Config(parser)
+
+    val lines = io.Source.fromPath(problemFilename).mkString
     val problem: Problem = Parser.fromText(lines)
+
     (new Simulation(config, problem)).run()
   }
 }
--- Darwin/src/main/scala/pl/poznan/put/darwin/model/Config.scala
+++ Darwin/src/main/scala/pl/poznan/put/darwin/model/Config.scala
@@ -1,42 +1,37 @@
 package pl.poznan.put.darwin.model
 
 import java.util.Random
+import org.ini4j.ConfigParser
 import pl.poznan.put.cs.idss.jrs.rules.VCDomLem
 
-class Config {
+class Config(parser: ConfigParser) {
 
-  val FILENAME: String = "problems/simple_1crit.mod"
-
-  val SOLUTION_COUNT: Int = 30
-  val SCENARIO_COUNT: Int = 1
-
-  val GOOD_COUNT: Int = 3
-
+  println(parser.sections())
+  println(parser.options("main"))
+  /*
+   * Main parameters
+   */
+  val SOLUTION_COUNT: Int = parser.getInt("main", "solutioncount")
+  val SCENARIO_COUNT: Int = parser.getInt("main", "scenariocount")
   val PERCENTILES: List[Int] = 1 :: 25 :: 50 :: Nil;
+  val DELTA: Double = parser.getDouble("main", "delta")
+  val GAMMA: Double = parser.getDouble("main", "gamma")
+  val MUTATION_TRIES: Int = parser.getInt("main", "mutationtries")
+  val ETA: Double = parser.getDouble("main", "eta")
+  val OMEGA: Double = parser.getDouble("main", "omega")
+  val USE_AVG: Boolean = parser.getBoolean("main", "useavg")
 
-  val DELTA = 0.1
-  val GAMMA = 2
-
-  val MUTATION_TRIES = 100
-
-  /**
-   *  Initial mutation probability
+  /*
+   * Mocked DM options
    */
-  val ETA = 0.5
-
-
-  /**
-   *  Mutate decay rate
-   */
-  val OMEGA = 0.1
-
-
+  val GOOD_COUNT: Int = parser.getInt("mockeddm", "goodcount")
+  
   /*
    * DOMLEM CONFIG
    */
   val DOMLEM_CONFIDECE_LEVEL = 1.0
   val CONDITION_SELECTION_METHOD = VCDomLem.MIX_CONDITIONS_FROM_DIFFERENT_OBJECTS
   val NEGATIVE_EXAMPLES_TREATMENT = VCDomLem.COVER_NONE_OF_NEGATIVE_EXAMPLES
-
-  val USE_AVG: Boolean = false
 }
+
+
