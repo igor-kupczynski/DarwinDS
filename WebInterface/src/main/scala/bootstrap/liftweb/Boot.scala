8a7ca26dff2ab509a1a9102ce7b1987f0fcbbbd5
Igor Kupczyński <puszczyk@gmail.com>
2010-08-01 13:05:07 +0200

    Workingo n web interface

--- WebInterface/src/main/scala/bootstrap/liftweb/Boot.scala
+++ WebInterface/src/main/scala/bootstrap/liftweb/Boot.scala
@@ -12,11 +12,6 @@ import mapper._
 
 import code.model._
 
-
-/**
- * A class that's instantiated early and run.  It allows the application
- * to modify lift's environment
- */
 class Boot {
   def boot {
     if (!DB.jndiJdbcConnAvailable_?) {
@@ -31,41 +26,25 @@ class Boot {
       DB.defineConnectionManager(DefaultConnectionIdentifier, vendor)
     }
 
-    // Use Lift's Mapper ORM to populate the database
-    // you don't need to use Mapper to use Lift... use
-    // any ORM you want
+
     Schemifier.schemify(true, Schemifier.infoF _, User)
 
-    // where to search snippet
     LiftRules.addToPackages("code")
 
-    // Build SiteMap
     val entries = List(
-      Menu.i("Home") / "index", // the simple way to declare a menu
+      Menu.i("Home") / "index",
+      Menu.i("Template") / "index_templ.html"
+    ) ::: User.sitemap
 
-      // more complex because this menu allows anything in the
-      // /static path to be visible
-      Menu(Loc("Static", Link(List("static"), true, "/static/index"), 
-	       "Static Content"))) :::
-    // the User management menu items
-    User.sitemap
-
-    // set the sitemap.  Note if you don't want access control for
-    // each page, just comment this line out.
     LiftRules.setSiteMap(SiteMap(entries:_*))
 
-    //Show the spinny image when an Ajax call starts
+
     LiftRules.ajaxStart =
       Full(() => LiftRules.jsArtifacts.show("ajax-loader").cmd)
-    
-    // Make the spinny image go away when it ends
     LiftRules.ajaxEnd =
       Full(() => LiftRules.jsArtifacts.hide("ajax-loader").cmd)
-
-    // Force the request to be UTF-8
+    
     LiftRules.early.append(_.setCharacterEncoding("UTF-8"))
-
-    // What is the function to test if a user is logged in?
     LiftRules.loggedInTest = Full(() => User.loggedIn_?)
 
     // Make a transaction span the whole HTTP request
