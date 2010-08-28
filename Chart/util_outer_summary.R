37018f6e543ad00e2a421ad800b7768468d15a82
Igor Kupczyński <puszczyk@gmail.com>
2010-08-28 23:19:57 +0200

    Working on simple performance

---
+++ Chart/util_outer_summary.R
@@ -0,0 +1,48 @@
+library("reshape")
+library("ggplot2")
+
+prep.data.line <- function(x, fname) {
+  innerM <- melt(x, id=c("test", "try", "outer"))
+  innerC <- cast(innerM, test + outer ~ variable, c(mean, sd))
+  innerC[c("test", "outer", "value_mean")]
+}
+
+gen.plot.line <- function(data,chart.title) {
+  c <- ggplot(data)
+  c <- c + geom_point(aes(outer, value_mean), colour="red")
+  c <- c + geom_line(aes(outer, value_mean), colour="red")
+  c <- c + opts(title=chart.title) 
+  c
+}
+
+optimal <- function(optimal, range) {
+  opt <- c(optimal)
+  outer <- 1:range
+  df <- data.frame(outer=outer, value=opt)
+  geom_line(aes(outer, value), data=df)
+}
+
+
+read.cmd.args <- function() {
+  n1 <- commandArgs()
+  n <- n1[length(n1)]
+  tmp<-strsplit(n,",")
+  args <- tmp[[1]]
+  if (length(args) == 5) {
+    return(args)
+  }
+  c(2, 4154.441453, "Pro-chart", "summary.csv", "util-outer-summary.pdf")
+}
+
+### MAIN ######################################################################
+args <- read.cmd.args()
+df <-read.csv(args[4], header=TRUE)
+pdf(args[5])
+data <- prep.data.line(df, args[3])
+c <- gen.plot.line(data, args[3])
+opt <- as.numeric(args[2])
+if (opt != 0) {
+  c <- c + optimal(opt, data$outer[length(data$outer)])
+}
+print(c)
+dev.off()
--- Experiment/genSummaryChart.sh
+++ Experiment/genSummaryChart.sh
@@ -5,19 +5,20 @@ function rel2abs {
     echo "`cd \`dirname $1\`; pwd`/`basename $1`"
 }
 
-EXPECTED_ARGS=3
+EXPECTED_ARGS=4
 E_BADARGS=65
 E_XCD=86
 
 if [ $# -ne $EXPECTED_ARGS ]
 then
-  echo "Usage: `basename $0` best-val charts-dir testplan-out-dir" >&2
+  echo "Usage: `basename $0` best-val charts-dir testplan-out-dir problem-name" >&2
   exit $E_BADARGS
 fi
 
 BEST=$1
 CHARTS_DIR=`rel2abs $2`
 TESTPLAN_OUT_DIR=`rel2abs $3`
+P_NAME=$4
 
 # We can set two criteria becasue brief option do not care
 ./genCharts.sh --brief 2 $BEST $CHARTS_DIR $TESTPLAN_OUT_DIR
@@ -35,5 +36,15 @@ echo "+--------------------------------------------------------------------+"
 DIR=$TESTPLAN_OUT_DIR
 R --slave --no-save --args "2,$BEST,$DIR/summary.csv,$DIR/summary_short.csv,$DIR/summary1.pdf,$DIR/summary2.pdf" \
     < $CHARTS_DIR/summary.R
+
+echo "+--------------------------------------------------------------------+"
+echo "|   GOT SUMMARY CHART                                                |"
+echo "+--------------------------------------------------------------------+"
+
+
+for D in `find $DIR -iname 'base'`
+do
+    R --slave --no-save --args "2,$BEST,$P_NAME,$D/summary.csv,$D/summary.pdf" < ../Chart/util_outer_summary.R
+done
 
 exit 0
