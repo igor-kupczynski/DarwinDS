dfe5039319af8ebb9762742b5f568761257a9c61
Igor Kupczyński <puszczyk@redcomrade.(none)>
2010-10-23 00:06:13 +0200

    A name change

--- Chart/util_outer.R
+++ Chart/util_outer.R
@@ -7,31 +7,32 @@ last <- function(x) {
 
 
 prep.data <- function(x) {
-  inner <- df[c("outer", "generation", "rank", "utility")]
-  innerM <- melt(inner, id=c("outer", "generation", "rank"))
-  innerC <- cast(innerM, outer + generation ~ variable, c(min, max))
-  innerM <- melt(innerC, id=c("outer", "generation"))
-  innerC <- cast(innerM, outer ~ variable, c(min, max, last))
+  inner <- df[c("exterior_iteration", "generation", "rank", "utility")]
+  innerM <- melt(inner, id=c("exterior_iteration", "generation", "rank"))
+  innerC <- cast(innerM, exterior_iteration + generation ~ variable, c(min, max))
+  innerM <- melt(innerC, id=c("exterior_iteration", "generation"))
+  innerC <- cast(innerM, exterior_iteration ~ variable, c(min, max))
   innerC
 }
 
 add.optimal <- function(plot, optimal, range) {
   opt <- c(optimal)
-  outer <- 1:range
-  df <- data.frame(outer=outer, value=opt)
-  plot <- plot +  geom_line(aes(outer, value), data=df)
+  exterior_iteration <- 1:range
+  df <- data.frame(exterior_iteration=exterior_iteration, value=opt)
+  plot <- plot +  geom_line(aes(exterior_iteration, value), data=df)
   plot
 }
 
 gen.plot <- function(data, filename) {
-  dataM <- melt(data, id=c("outer"))
+  dataM <- melt(data, id=c("exterior_iteration"))
   write.table(data, file=filename, sep=",", row.names=F)
+  dataM$result_value <- dataM$result_variable
   c <- ggplot(dataM)
-  c <- c + geom_ribbon(aes(outer, ymin=utility_min, ymax=utility_max),
+  c <- c + geom_ribbon(aes(exterior_iteration, ymin=utility_min, ymax=utility_max),
                        data=data, alpha=0.2)
-  c <- c + geom_point(aes(outer, value, colour=result_variable))
-  c <- c + geom_line(aes(outer, value, colour=result_variable))
-  c <- c + opts(title="Utility/Outer")
+  c <- c + geom_point(aes(exterior_iteration, value, colour=result_value))
+  c <- c + geom_line(aes(exterior_iteration, value, colour=result_value))
+  c <- c + opts(title="Supposed Utility")
   c
 }
 
@@ -50,6 +51,7 @@ read.cmd.args <- function() {
 args <- read.cmd.args()
 
 df <-read.csv(args[3], header=TRUE)
+df <- rename(df, c(outer="exterior_iteration"))
 pdf(args[4])
 data <- prep.data(df)
 c <- gen.plot(data, args[5])
