0494a0314ac2e88d005d557c28753d8dc6953389
Igor Kupczyński <puszczyk@gmail.com>
2010-11-01 19:01:03 +0100

    Make dist update

--- make_dist.sh
+++ make_dist.sh
@@ -6,8 +6,10 @@ cd ..
 ls Darwin/target/scala_2.8.0/darwin_2.8.0* > OneJar/darwin-version.txt
 cp Darwin/target/scala_2.8.0/darwin_2.8.0*.jar OneJar/main/darwin.jar
 
+
 cd OneJar
 jar -cvfm ../Distribution/darwin-full.jar boot-manifest.mf .
 cd ..
+chmod +x Distribution/darwin-full.jar
 
 zip -r Distribution.zip Distribution
